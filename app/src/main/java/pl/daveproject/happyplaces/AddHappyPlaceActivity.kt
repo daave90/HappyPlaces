c2e2ce6c7f82ab90bd6f2f5889b4151caa918d2a
Dave <maciak.dawid.96@gmail.com>
2021-03-25 20:24:41 +0100

    create add happy place ui

--- app/src/main/java/pl/daveproject/happyplaces/AddHappyPlaceActivity.kt
+++ app/src/main/java/pl/daveproject/happyplaces/AddHappyPlaceActivity.kt
@@ -1,11 +1,24 @@
 package pl.daveproject.happyplaces
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class AddHappyPlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_happy_place)
+        createToolbarWithBackButton()
+    }
+
+    private fun createToolbarWithBackButton() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_add_place)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.title = resources.getString(R.string.addHappyPlaceToolbarTitle)
+        }
+        toolbar.setNavigationOnClickListener { onBackPressed() }
     }
 }

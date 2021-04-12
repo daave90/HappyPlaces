7e6682b1b02d6b2484bad0e2d0fca7e315dacb2b
Dave <maciak.dawid.96@gmail.com>
2021-04-12 13:11:53 +0200

    Add details toolbar

---
+++ app/src/main/java/pl/daveproject/happyplaces/activity/HappyPlaceDetailActivity.kt
@@ -0,0 +1,24 @@
+package pl.daveproject.happyplaces.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import pl.daveproject.happyplaces.R
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+
+    private fun createToolbarWithBackButton() {
+        val toolbar = findViewById<Toolbar>(R.id.toolbar_happy_place_detail)
+        setSupportActionBar(toolbar)
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.title = resources.getString(R.string.happyPlaceDetailToolbarTitle)
+        }
+        toolbar.setNavigationOnClickListener { onBackPressed() }
+    }
+}
